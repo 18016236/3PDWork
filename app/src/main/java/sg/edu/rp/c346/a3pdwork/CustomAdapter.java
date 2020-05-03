c650584fd577fe0b959c921a8ad4a0b40157f4af
18016236 <18016236@myrp.edu.sg>
2020-05-03 13:50:18 +0800

    4th updated version of 3rd app

--- app/src/main/java/sg/edu/rp/c346/a3pdwork/CustomAdapter.java
+++ app/src/main/java/sg/edu/rp/c346/a3pdwork/CustomAdapter.java
@@ -18,51 +18,44 @@ public class CustomAdapter extends ArrayAdapter {
     private TextView tvname;
    private TextView Contact;
     private TextView tvrate;
+    private TextView tvspeciality;
     private TextView tvrecommendations;
     private ImageView pic;
     private Context context;
 
     public CustomAdapter(Context context, int resource, ArrayList<Details> objects) {
         super(context,resource,objects);
-        // Store the food that is passed to this adapter
         detailsArrayList = objects;
-        // Store Context object as we would need to use it later
         this.context = context;
 
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // The usual way to get the LayoutInflater object to
-        //  "inflate" the XML file into a View object
+
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        // "Inflate" the row.xml as the layout for the View object
         View rowView = inflater.inflate(R.layout.row, parent, false);
 
 
-        // Get the TextView object
         Contact = (TextView) rowView.findViewById(R.id.textViewphoneNum);
         tvname = (TextView) rowView.findViewById(R.id.textViewName);
         tvrate = (TextView) rowView.findViewById(R.id.textviewrate);
+        tvspeciality = (TextView) rowView.findViewById(R.id.textviewSpeciality);
         tvrecommendations = (TextView) rowView.findViewById(R.id.textViewRecommendations);
 
-        // Get the ImageView object
         pic = (ImageView) rowView.findViewById(R.id.imageView);
 
-        // The parameter "position" is the index of the
-        //  row ListView is requesting.
-        //  We get back the food at the same index.
+
         Details currentDays = detailsArrayList.get(position);
-        // Set the TextView to show the food
 
         tvname.setText(currentDays.getName());
         Contact.setText(currentDays.getPhoneNum());
         tvrate.setText(currentDays.getRate());
+        tvspeciality.setText(currentDays.getSpeciality());
         tvrecommendations.setText(currentDays.getRecommendations());
         pic.setImageResource(currentDays.getPic());
 
-        // Return the nicely done up View to the ListView
         return rowView;
     }
 }
--- app/src/main/java/sg/edu/rp/c346/a3pdwork/rowActivity.java
+++ app/src/main/java/sg/edu/rp/c346/a3pdwork/rowActivity.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 public class rowActivity extends AppCompatActivity {
     ListView lv;
     ArrayAdapter aa;
-    ArrayList<Details> courses, row,details;
+    ArrayList<Details> courses,English,Math,Chinese,Biology,Chemistry,Physics,Python,Java,SQL,Swift,Accounting,Additional,History;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,29 +25,117 @@ public class rowActivity extends AppCompatActivity {
         lv = findViewById(R.id.listViewDetails);
 
         courses = new ArrayList<Details>();
-        row = new ArrayList<Details>();
-        details = new ArrayList<Details>();
+        English = new ArrayList<Details>();
+        Math = new ArrayList<Details>();
+        Chinese = new ArrayList<Details>();
+        Biology = new ArrayList<Details>();
+        Chemistry = new ArrayList<Details>();
+        Physics = new ArrayList<Details>();
+        Python = new ArrayList<Details>();
+        Java = new ArrayList<Details>();
+        SQL = new ArrayList<Details>();
+        Swift = new ArrayList<Details>();
+        Accounting = new ArrayList<Details>();
+        Additional = new ArrayList<Details>();
+        History = new ArrayList<Details>();
 
-        row.add(new Details(R.drawable.male, "Mervin", "98765432","$50/hr","English/Chinese/Math/Additional Math/History/Java/Python","He is an Amazing tutor!"));
-        details.add(new Details( R.drawable.male,"John", "99123467","$70/hr","Chinese/Math","My kid loves him!"));
-        details.add(new Details( R.drawable.male, "Gary","90874512","$85/hr","Chinese/History" ,"He takes great effort in teaching the kids!"));
-        row.add(new Details( R.drawable.male, "Nathan","93451820","$2500/hr","English/Math/Addtional Math/Java/Python/SQL/Swift","Overpriced tutor!!"));
-        row.add(new Details(R.drawable.male,"Jian Tong", "90125837","$500/hr","English/Java/Python/SQL/Swift","He is quite good as a tutor"));
-        row.add(new Details( R.drawable.female, "Syafiqah","97563482","$1500/hr","English/Java/Python","Booo! So overpriced!!"));
-        row.add(new Details(R.drawable.female, "Shu jing","99999999","$80/hr","English/Chinese","Okay service for the price"));
-        row.add(new Details( R.drawable.female, "Mary","92857645","$90/hr","English/Math","She is not a very good tutor"));
-        row.add(new Details( R.drawable.male, "Zhen nan","92481720","$150/hr","English/Chinese/Java/Python","He is very well-mannered"));
-        row.add(new Details( R.drawable.male, "Yi xuan","91208917","$870/hr","English/Math/Chinese/Biology/Chemistry","He teaches well"));
-        // Link this Activity object, the row.xml layout for
-        //  each row and the food String array together
+
+        English.add(new Details(R.drawable.male, "Mervin", "98765432","$50/hr","English/Chinese/Math/Additional Math/History/Java/Python","He is an Amazing tutor!"));
+        English.add(new Details( R.drawable.male, "Nathan","93451820","$2500/hr","English/Math/Addtional Math/Java/Python/SQL/Swift","Overpriced tutor!!"));
+        English.add(new Details(R.drawable.male,"Jian Tong", "90125837","$500/hr","English/Java/Python/SQL/Swift","He is quite good as a tutor"));
+        English.add(new Details( R.drawable.female, "Syafiqah","97563482","$1500/hr","English/Java/Python","Booo! So overpriced!!"));
+        English.add(new Details(R.drawable.female, "Shu jing","99999999","$80/hr","English/Chinese","Okay service for the price"));
+        English.add(new Details( R.drawable.female, "Mary","92857645","$90/hr","English/Math","She is not a very good tutor"));
+        English.add(new Details( R.drawable.male, "Zhen nan","92481720","$150/hr","English/Chinese/Java/Python","He is very well-mannered"));
+        English.add(new Details( R.drawable.male, "Yi xuan","91208917","$870/hr","English/Math/Chinese/Biology/Chemistry","He teaches well"));
+
+        Math.add(new Details( R.drawable.male,"John", "99123467","$70/hr","Chinese/Math","My kid loves him!"));
+        Math.add(new Details( R.drawable.male, "Gary","90874512","$85/hr","Chinese/History/Math" ,"He takes great effort in teaching the kids!"));
+
+        Chinese.add(new Details( R.drawable.male,"Yi xuan", "99123467","$70/hr","English/Math/Chinese/Biology/Chemistry","My kid loves him!"));
+        Chinese.add(new Details( R.drawable.female, "Shu Jing","90874512","$85/hr","Chinese/History/Math/Python" ,"He takes great effort in teaching the kids!"));
+        Chinese.add(new Details( R.drawable.male,"Zhen nan", "99123467","$70/hr","English/Chinese/Java/Python","My kid loves him!"));
+        Chinese.add(new Details( R.drawable.female, "Yujia","90874512","$85/hr","Chinese/History/Math" ,"He takes great effort in teaching the kids!"));
+        Chinese.add(new Details(R.drawable.male, "Mervin", "98765432","$50/hr","English/Chinese/Math/Additional Math/History/Java/Python","He is an Amazing tutor!"));
+
+        Biology.add(new Details( R.drawable.male, "Yi xuan","91208917","$870/hr","English/Math/Chinese/Biology/Chemistry","He teaches well"));
+
+        Chemistry.add(new Details( R.drawable.male, "Yi xuan","91208917","$870/hr","English/Math/Chinese/Biology/Chemistry","He teaches well"));
+
+        Physics.add(new Details(R.drawable.male, "Peter", "99999999","$70/hr","Physics","He is an Amazing tutor!" + " My Kids grades have improved under him"));
+        Physics.add(new Details( R.drawable.male, "Larry","98888888","$1500/hr","Physics","Overpriced tutor!!"+" He gives a really bad service for that price"));
+        Physics.add(new Details(R.drawable.male,"Tim", "97777777","$300/hr","Physics","He is quite good as a tutor"+" He is really patient, my kids love him"));
+        Physics.add(new Details( R.drawable.female, "Maegan","97666666","$2500/hr","Physics","Booo! So overpriced!!"+" Charges so much for his service and yet my kids dont have an improvement in their own grades"));
+        Physics.add(new Details(R.drawable.female, "Melinda","96666666","$10/hr","Physics","Mediocore service for the price"));
+        Physics.add(new Details( R.drawable.female, "Melissa","95555555","$80/hr","Physics","She is not a very good tutor"+" She is really strict with the kids causing my kids to be terrified of her"));
+        Physics.add(new Details( R.drawable.male, "Bill","94444444","$350/hr","Physics","He is very well-mannered"+" He is really gentle with the children"));
+        Physics.add(new Details( R.drawable.male, "Jack","93333333","$170/hr","Physics","He teaches well"+" Excellent tutor!!"));
+
+
+        Python.add(new Details(R.drawable.male, "Mervin", "98765432","$50/hr","English/Chinese/Math/Additional Math/History/Java/Python","He is an Amazing tutor!"));
+        Python.add(new Details( R.drawable.male, "Nathan","93451820","$2500/hr","English/Math/Addtional Math/Java/Python/SQL/Swift","Overpriced tutor!!"));
+        Python.add(new Details(R.drawable.male,"Jian Tong", "90125837","$500/hr","English/Java/Python/SQL/Swift","He is quite good as a tutor"));
+        Python.add(new Details( R.drawable.female, "Syafiqah","97563482","$1500/hr","English/Java/Python","Booo! So overpriced!!"));
+        Python.add(new Details( R.drawable.male, "Zhen nan","92481720","$150/hr","English/Chinese/Java/Python","He is very well-mannered"));
+
+        Java.add(new Details(R.drawable.male, "Mervin", "98765432","$50/hr","English/Chinese/Math/Additional Math/History/Java/Python","He is an Amazing tutor!"));
+        Java.add(new Details( R.drawable.male, "Nathan","93451820","$2500/hr","English/Math/Addtional Math/Java/Python/SQL/Swift","Overpriced tutor!!"));
+        Java.add(new Details(R.drawable.male,"Jian Tong", "90125837","$500/hr","English/Java/Python/SQL/Swift","He is quite good as a tutor"));
+        Java.add(new Details( R.drawable.female, "Syafiqah","97563482","$1500/hr","English/Java/Python","Booo! So overpriced!!"));
+        Java.add(new Details( R.drawable.male, "Zhen nan","92481720","$150/hr","English/Chinese/Java/Python","He is very well-mannered"));
+
+        SQL.add(new Details( R.drawable.male, "Nathan","93451820","$2500/hr","English/Math/Addtional Math/Java/Python/SQL/Swift","Overpriced tutor!!"));
+        SQL.add(new Details(R.drawable.male,"Jian Tong", "90125837","$500/hr","English/Java/Python/SQL/Swift","He is quite good as a tutor"));
+
+        Swift.add(new Details( R.drawable.male, "Nathan","93451820","$2500/hr","English/Math/Addtional Math/Java/Python/SQL/Swift","Overpriced tutor!!"));
+        Swift.add(new Details(R.drawable.male,"Jian Tong", "90125837","$500/hr","English/Java/Python/SQL/Swift","He is quite good as a tutor"));
+
+        Accounting.add(new Details(R.drawable.male, "Harry", "90000000","$50/hr","Accounting","He is a dedicated tutor"));
+        Accounting.add(new Details( R.drawable.male, "William","93111111","$2500/hr","Accounting","Overpriced tutor!! Not worth to engage his services"));
+        Accounting.add(new Details(R.drawable.male,"Phillip", "92222222","$500/hr","Accounting","My child has become more confident under him"));
+        Accounting.add(new Details( R.drawable.female, "Kateson","97709238","$1500/hr","Accounting","She does not even seem to know her stuff at all, the audacity for the price"));
+        Accounting.add(new Details( R.drawable.male, "Charles","92482819","$150/hr","Accounting","He is extremely well-mannered"));
+
+        Additional.add(new Details(R.drawable.male, "Mervin", "98765432","$50/hr","English/Chinese/Math/Additional Math/History/Java/Python","He is an Amazing tutor!"));
+        Additional.add(new Details( R.drawable.male, "Nathan","93451820","$2500/hr","English/Math/Addtional Math/Java/Python/SQL/Swift","Overpriced tutor!!"));
+
+        History.add(new Details( R.drawable.male,"Yi xuan", "99123467","$70/hr","English/Math/Chinese/Biology/Chemistry","My kid loves him!"));
+        History.add(new Details( R.drawable.female, "Shu Jing","90874512","$85/hr","Chinese/History/Math/Python" ,"He takes great effort in teaching the kids!"));
+        History.add(new Details( R.drawable.male,"Zhen nan", "99123467","$70/hr","English/Chinese/Java/Python","My kid loves him!"));
+        History.add(new Details( R.drawable.female, "Yujia","90874512","$85/hr","Chinese/History/Math" ,"He takes great effort in teaching the kids!"));
+        History.add(new Details(R.drawable.male, "Mervin", "98765432","$50/hr","English/Chinese/Math/Additional Math/History/Java/Python","He is an Amazing tutor!"));
+        History.add(new Details( R.drawable.male, "Gary","90874512","$85/hr","Chinese/History/Math" ,"He takes great effort in teaching the kids!"));
+
 
         Intent i = getIntent();
         int number = i.getIntExtra("number", 0);
 
         if (number == 0) {
-            courses = row;
-        } else {
-            courses = details;
+            courses = English;
+        } else if (number == 1){
+            courses = Math;
+        }else if (number == 2){
+            courses = Chinese;
+        }else if (number == 3){
+            courses = Biology;
+        }else if (number == 4) {
+            courses = Chemistry;
+        }else if (number == 5) {
+            courses = Physics;
+        }else if (number == 6) {
+            courses = Python;
+        }else if (number == 7) {
+            courses = Java;
+        }else if (number == 8) {
+            courses = SQL;
+        }else if (number == 9) {
+            courses = Swift;
+        }else if (number == 10) {
+            courses = Accounting;
+        }else if (number == 11) {
+            courses = Additional;
+        }else {
+            courses = History;
         }
 
         aa = new CustomAdapter(this, R.layout.row, courses);
@@ -61,7 +149,7 @@ public class rowActivity extends AppCompatActivity {
                 Details selected = courses.get(position);
 
                 Toast.makeText(rowActivity.this,"You have selected: "+ selected.getName()
-                                + " Contact: " + selected.getPhoneNum() + " Rate: " + selected.getRate() + " Recommendations: " + selected.getRecommendations(),
+                                + " Contact: " + selected.getPhoneNum() + " Rate: " + selected.getRate() + " Speciality: " + selected.getSpeciality() +" Recommendations: " + selected.getRecommendations(),
                         Toast.LENGTH_LONG).show();
             }
         });
